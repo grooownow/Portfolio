1f99c4f62eb2951fa21db2a7dbda1cd1db6674e4
Ekaterina Pustovaya <krobox@gmail.com>
2017-05-06 20:06:18 +0300

    changed info

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,10 +7,10 @@ module.exports = function() {
       .pipe($.gp.sassGlob())
       .pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
       .pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+    .pipe($.gp.cssUnit({
+        type: 'px-to-rem'
+    }))
       .pipe($.gp.sourcemaps.write())
-        .pipe($.gp.cssUnit({
-            type: 'px-to-rem'
-        }))
       .pipe($.gulp.dest($.config.root + '/assets/css'))
       .pipe($.browserSync.stream());
   })
